56fa54e109f91133b1f6a110ac47ee9123517610
Will <w.alexander.johnson@gmail.com>
2020-01-02 10:53:25 -0600

    new post

--- content/posts/text/button.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const Button = () => {
-    const [ count, setCount ] = React.useState(0)
-    return <button onClick={()=> setCount(count + 1)}>click({count})</button>
-}
-
-export default Button
